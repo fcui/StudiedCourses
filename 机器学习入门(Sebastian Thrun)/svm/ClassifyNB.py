8b3774ba927ed4b6099c00f67e6d6c08e2ecee67
fcui <fcui@zjgsu.edu.cn>
2017-06-14 15:38:58 +0800

    测试3.22中选择SVM的不同C的情况

---
+++ 机器学习入门(Sebastian Thrun)/svm/ClassifyNB.py
@@ -0,0 +1,15 @@
+# 利用SVM测试3.22的习题
+def classify(features_train, labels_train):
+    # import the sklearn module for GaussianNB
+    # create classifier
+    # fit the classifier on the training features and labels
+    # return the fit classifier
+
+    # your code goes here!
+    # from sklearn.naive_bayes import GaussianNB
+    from sklearn import svm
+
+    # clf = GaussianNB()
+    clf = svm.SVC(C=10000)
+    clf.fit(features_train, labels_train)
+    return clf
